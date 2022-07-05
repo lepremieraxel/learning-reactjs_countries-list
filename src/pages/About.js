8732c98683ad2a4c2918fd913fd9ba213aea2cc5
Axel Marcial <axelmarcial151003@gmail.com>
2022-07-06 01:49:39 +0200

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,54 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, laborum
+        doloremque tenetur quisquam dignissimos deleniti sed eos officiis
+        dolorum illum et iure nihil vel ut modi fuga sunt debitis quibusdam.
+        Aspernatur similique consequuntur perferendis natus eos provident
+        pariatur cum quae molestias incidunt, soluta laborum facere doloribus
+        ipsum aliquam. Fugit, repellendus incidunt accusamus fugiat temporibus
+        consectetur voluptatibus et facere suscipit obcaecati natus facilis
+        maiores omnis quaerat veniam illo error odio quibusdam porro possimus
+        molestias! Modi sunt dolorum soluta molestiae obcaecati assumenda a
+        voluptatibus officiis vero unde quo optio, est tempore magni nesciunt ex
+        commodi nemo consequatur rem error, cum molestias atque?
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus
+        quas eius officia odio magni itaque et ut exercitationem maiores iure,
+        dolores blanditiis error, ipsa vel enim nam natus tenetur dolore qui,
+        voluptas cumque facere? Minus cumque, ipsum totam exercitationem dolorum
+        doloribus neque voluptate eos molestiae blanditiis ut maiores maxime
+        pariatur? Magnam iste fugit vero nam necessitatibus quisquam possimus
+        culpa. Corrupti harum totam doloribus rerum pariatur? Impedit recusandae
+        maxime dolorum asperiores. Enim saepe officia quas sapiente reiciendis
+        qui, nesciunt facere ipsam ipsa, minus illum facilis vitae ut libero
+        quis amet optio nisi sit explicabo deserunt id. Et ducimus quia at
+        quidem aliquid aut dolorum unde? Sed inventore laboriosam necessitatibus
+        quasi, obcaecati laudantium, accusamus at accusantium corrupti enim
+        expedita blanditiis minus molestias qui velit numquam beatae nesciunt
+        atque. Commodi quas laudantium ut ducimus magni itaque aliquid sequi,
+        doloribus doloremque debitis fugiat minus ipsam iste veritatis. Eligendi
+        amet quisquam, vitae voluptates officia aspernatur corporis ipsa unde id
+        est aliquam quae molestias repudiandae, cumque dolor ut magni!
+        Aspernatur eveniet repellendus voluptatum reprehenderit libero
+        temporibus error atque quibusdam veritatis ullam veniam dicta, doloribus
+        pariatur nobis maiores porro laborum architecto officia modi? Et
+        consequatur fugit incidunt numquam odit libero est nam qui culpa
+        quisquam! Dolorem, accusantium!
+      </p>
+    </div>
+  );
+};
+
+export default About;
